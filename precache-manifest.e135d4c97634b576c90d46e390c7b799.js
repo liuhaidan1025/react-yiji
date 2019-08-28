586f154c37988bcadbdecff6afa63fea03ed87ad
liuhaidan1025 <80543957@qq.comgit config --global user.name  liuhaidan1025git config --global user.email 80543957@qq.com>
2019-08-28 18:44:11 +0800

    Updates

---
+++ precache-manifest.e135d4c97634b576c90d46e390c7b799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89fa7fe04861b7ed32c1a6399605dc8",
+    "url": "/react-yiji/index.html"
+  },
+  {
+    "revision": "40c708b2686ace87f631",
+    "url": "/react-yiji/static/css/2.acb72abc.chunk.css"
+  },
+  {
+    "revision": "e6a1641d3e6e748c0a1d",
+    "url": "/react-yiji/static/css/main.7f882da5.chunk.css"
+  },
+  {
+    "revision": "40c708b2686ace87f631",
+    "url": "/react-yiji/static/js/2.9c3762f5.chunk.js"
+  },
+  {
+    "revision": "e6a1641d3e6e748c0a1d",
+    "url": "/react-yiji/static/js/main.5cf11f3a.chunk.js"
+  },
+  {
+    "revision": "76e3e25619ead56721a2",
+    "url": "/react-yiji/static/js/runtime~main.df17a495.js"
+  }
+]);
